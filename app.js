78329326116b1316ae553abb04632255a6e1af52
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-08 17:21:22 +0300

    INIT: express app

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
+import 'dotenv/config';
 import express from 'express';
-import { getRoutes } from './routes/index.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import cors from 'cors';
+import getRoutes from './routes/index.js';
+import './config/database.js';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+/**
+ * session config
+ */
+app.use(
+  session({
+    name: process.env.SESSION_NAME,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    // store: MongoStore.create({mongoUrl: }),
+    cookie: {
+      maxAge: Number(process.env.SESSION_LIFETIME),
+      sameSite: true,
+    },
+  }),
+);
 
 app.use('/api', getRoutes);
 
-const PORT = 8080;
-
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`server is running on port ${process.env.PORT}`));
