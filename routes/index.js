20c64c0a06571b2283425fb7e64f333fe7279af8
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-08 17:24:25 +0300

    gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,50 @@
 import express from 'express';
+import { createUser } from '../services/createUser.js';
 
 const router = express.Router();
 
-export function getRoutes() {
-  return router;
-}
+router.post('/', async (req, res, next) => {
+  const errors = [];
+  const { email, password, confirmPassword } = req.body;
+
+  /**
+   * form validation
+   */
+  if (password !== confirmPassword) {
+    errors.push({
+      message: 'Please ensure the passwords are similar',
+    });
+  }
+
+  if (password.length <= 8) {
+    errors.push({
+      message: 'Please ensure the password is more than 8 characters',
+    });
+  }
+
+  /**
+   * check for validation errors
+   */
+  if (errors.length > 0) {
+    res.status(400).json({ errors });
+  } else {
+    /**
+     * if validation passes return new user
+     */
+    const result = await createUser({ username: email, password });
+
+    /**
+     * checks if email is already in use
+     */
+    if (typeof result === 'object') {
+      errors.push(result);
+      console.log(result);
+      res.status(400).json({ errors });
+      return;
+    }
+
+    res.status(201).json({ ID: result });
+  }
+});
+
+export default router;
